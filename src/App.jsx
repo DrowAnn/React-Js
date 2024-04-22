d9a74024ed2eebdd54bbd7711d2437aeff913423
Robert Steven Delgado Peralta <rs19990801@gmail.com>
2024-04-21 21:26:06 -0500

    Creacion de ramas Clase-1 y Clase-2

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,7 @@ import "./App.css";
 import Button from "./components/Button";
 
 function App() {
-  const saludo = "Hola";
-  const nombre = "Robert";
-
-  return (
-    <>
-      <h1>Bienvenido!</h1>
-      <h2 className="h2-title">{`${saludo}, ${nombre}`}</h2>
-      <Button
-        onClick={() => {
-          console.log("Hizo Click");
-        }}
-      >
-        Clickeame!
-      </Button>
-      <Button>Segundo Botón</Button>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
