39882dcb8f5985d6ff572c485705eaccda9b4cc4
DrowAnn <rs19990801@gmail.com>
2024-04-23 16:15:26 -0500

    Avance  Clase-5

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,52 @@ import Boton from "./components/Boton/index";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [cargaBoton, setCargaBoton] = useState(false);
   const [listaProductos, setListaDeProductos] = useState([]);
+  const [tituloLista, setTituloLista] = useState(false);
+  const [bienvenida, setBienvenida] = useState(true);
+
+  // useEffect(() => {
+  //   console.log("Montaje");
+
+  // fetch("https://fakestoreapi.com/products")
+  //   .then((res) => res.json())
+  //   .then((json) => {
+  //     setTimeout(() => {
+  //       setListaDeProductos(json);
+  //       setIsLoading(false);
+  //     }, 2000);
+  //   });
+
+  //   return () => {
+  //     console.log("Desmontaje");
+  //   };
+  // }, []);
+
+  // useEffect(() => {
+  //   if (listaProductos.length) {
+  //     alert("La lista ha sido cargada con exito!");
+  //   }
+  // }, [listaProductos]);
+
+  // if (isLoading) {
+  //   return <p>Loading...</p>;
+  // }
 
   useEffect(() => {
-    console.log("Montaje");
-
-    fetch("https://fakestoreapi.com/products")
-      .then((res) => res.json())
-      .then((json) => {
-        setTimeout(() => {
-          setListaDeProductos(json);
-          setIsLoading(false);
-        }, 2000);
-      });
-
-    return () => {
-      console.log("Desmontaje");
-    };
+    setTimeout(() => {
+      setBienvenida(false);
+      setIsLoading(true);
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1500);
+    }, 2500);
   }, []);
 
-  useEffect(() => {
-    if (listaProductos.length) {
-      alert("La lista ha sido cargada con exito!");
-    }
-  }, [listaProductos]);
+  if (bienvenida) {
+    return <h1>Bienvenido</h1>;
+  }
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -35,7 +56,36 @@ function App() {
 
   return (
     <>
-      <h1>Lista de Productos</h1>
+      <h1>Manejo de Lista</h1>
+      <Boton
+        color="Blue"
+        onClick={() => {
+          setCargaBoton(true);
+          fetch("https://fakestoreapi.com/products")
+            .then((res) => res.json())
+            .then((json) => {
+              setTimeout(() => {
+                setListaDeProductos(json);
+                setCargaBoton(false);
+                setTituloLista(true);
+              }, 1500);
+            });
+        }}
+        cargando={cargaBoton}
+      >
+        Imprimir Lista
+      </Boton>
+      <Boton
+        color="Red"
+        onClick={() => {
+          setListaDeProductos([]);
+          setTituloLista(false);
+          // alert("Se eliminara la visra de los productos");
+        }}
+      >
+        Borrar Lista
+      </Boton>
+      <h1>{tituloLista ? "Lista de Productos" : ""}</h1>
       {listaProductos.map((product, index) => {
         return (
           <div key={index.toString()}>
--- src/components/Boton/index.jsx
+++ src/components/Boton/index.jsx
@@ -1,11 +1,26 @@
 import "./boton.css";
+import PropTypes from "prop-types";
 
 const Boton = (props) => {
   return (
     <>
-      <button className="boton">{props.children}</button>
+      <button
+        className="boton"
+        style={{ backgroundColor: props.color }}
+        onClick={props.onClick}
+        disabled={props.cargando}
+      >
+        {props.cargando ? "Loading..." : props.children}
+      </button>
     </>
   );
+};
+
+Boton.propTypes = {
+  children: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
+  cargando: PropTypes.bool,
 };
 
 export default Boton;
