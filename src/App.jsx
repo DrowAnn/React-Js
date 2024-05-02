1db3b0d2ef7a3a54a10c50141b1aba8f97188e71
DrowAnn <rs19990801@gmail.com>
2024-05-02 08:38:34 -0500

    Clase 7. States

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useContext, useState } from "react";
 import React from "react";
 import "./App.css";
 
-const userContext = React.createContext();
-
 const Contador = () => {
   const [contador, setContador] = useState(0);
 
@@ -21,43 +19,10 @@ const Contador = () => {
   );
 };
 
-const Encabezado = () => {
-  const User = useContext(userContext);
-
-  return (
-    <div>
-      <h1>Bienvenido</h1>
-      <div>
-        <img src={User.foto} style={{ width: "100px" }} />
-      </div>
-    </div>
-  );
-};
-
-const Perfil = () => {
-  const User = useContext(userContext);
-
-  return (
-    <div>
-      <h2>Nombre: {User.nombre}</h2>
-      <h2>Apellido: {User.apellido}</h2>
-    </div>
-  );
-};
-
 function App() {
-  const User = {
-    nombre: "Robert",
-    apellido: "Delgado",
-    foto: "https://static.vecteezy.com/system/resources/previews/002/275/847/non_2x/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg",
-  };
-
   return (
     <>
-      <userContext.Provider value={User}>
-        <Encabezado />
-        <Perfil />
-      </userContext.Provider>
+      <Contador />
     </>
   );
 }
