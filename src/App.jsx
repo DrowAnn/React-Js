a0e24a26fb0e3bebe148c33214b2c7d85901f56f
DrowAnn <rs19990801@gmail.com>
2024-05-02 08:42:50 -0500

    Clase 6. Props changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,45 @@ import { useContext, useState } from "react";
 import React from "react";
 import "./App.css";
 
-const Contador = () => {
-  const [contador, setContador] = useState(0);
+const userContext = React.createContext();
+
+const Encabezado = () => {
+  const User = useContext(userContext);
 
   return (
     <div>
-      <h1>Cantidad de Clicks</h1>
-      <button
-        onClick={() => {
-          setContador(contador + 1);
-        }}
-      >
-        {contador}
-      </button>
+      <h1>Bienvenido</h1>
+      <div>
+        <img src={User.foto} style={{ width: "100px" }} />
+      </div>
+    </div>
+  );
+};
+
+const Perfil = () => {
+  const User = useContext(userContext);
+
+  return (
+    <div>
+      <h2>Nombre: {User.nombre}</h2>
+      <h2>Apellido: {User.apellido}</h2>
     </div>
   );
 };
 
 function App() {
+  const User = {
+    nombre: "Robert",
+    apellido: "Delgado",
+    foto: "https://static.vecteezy.com/system/resources/previews/002/275/847/non_2x/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg",
+  };
+
   return (
     <>
-      <Contador />
+      <userContext.Provider value={User}>
+        <Encabezado />
+        <Perfil />
+      </userContext.Provider>
     </>
   );
 }
