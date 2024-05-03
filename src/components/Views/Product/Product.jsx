854894379c9ba7cd709b6d365a11911ec76b65a8
DrowAnn <rs19990801@gmail.com>
2024-05-03 18:48:08 -0500

    Avance Clase-12

--- src/components/Views/Product/Product.jsx
+++ src/components/Views/Product/Product.jsx
@@ -1,12 +1,34 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
-const Product = () => {
+const Product = (props) => {
+  const [Loading, setLoading] = useState(true);
   const params = useParams();
-  console.log(params);
+  const [list, setList] = useState([]);
+  let arreglo = [];
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => {
+        setList(json);
+        setLoading(false);
+      });
+  }, []);
+
+  if (Loading) {
+    return <h1>Is Loading...</h1>;
+  }
+
+  useEffect(() => {
+    arreglo = [...list];
+    console.log(arreglo);
+  }, [list]);
 
   return (
-    <div>
-      <h1>Camisa</h1>
+    <div className={props.hide}>
+      <Link to="/products">Volver</Link>
+      <h1>{!Loading && console.log(arreglo)}</h1>
     </div>
   );
 };
--- src/components/Views/Products/Products.jsx
+++ src/components/Views/Products/Products.jsx
@@ -1,13 +1,33 @@
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Product from "../Product/Product";
+
+export const ListContext = React.createContext();
 
 const Products = () => {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => {
+        setList(json);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>Lista de Productos</h1>
       <Link to="/">Home</Link>
-      <div>
-        <Link to="/product/0">Ver producto</Link>
-      </div>
+      <h1>Lista de Productos</h1>
+      <ol>
+        {list.map((product) => {
+          return (
+            <li key={product.id}>
+              <Link to={`/product/${product.id}`}>{product.title}</Link>
+            </li>
+          );
+        })}
+      </ol>
     </div>
   );
 };
